1b3e357314037c9617e74714ea88933b4913815d
nsonkwai <61365717+nsonkwai@users.noreply.github.com>
2020-02-26 19:16:43 -0700

    Change add() and remove().
    Add() method now adds a new element to the beginning and if the list is
    full, it increases the size by 50 percent.
    
    Remove() method now removes an element and decreases the size by 25
    percent if there are more than 25 percent empty spaces.

--- cse360assign2/src/cse360assign2/SimpleList.java
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -11,17 +11,20 @@
  */
 package cse360assign2;
 
+import java.util.Arrays;
+
 /**
  * 
  * Public class SimpleList holds methods that perform operations on a list.
  * These operations consist of creating a new list, adding an element to the
  * beginning of the list, removing an element, counting the amount of elements,
- * search through the list, and printing the list.
+ * search through the list, printing the list, appending an element to the end
+ * of the list, returning the first element, and returning the size of the list.
  *
  */
 public class SimpleList {
-	private int[] list;
-	private int count;
+	private static int[] list;
+	private static int count;
 
 	/**
 	 * 
@@ -36,16 +39,17 @@ public class SimpleList {
 
 	/**
 	 * This method adds the new element to the beginning of the list. If the list is
-	 * full, the last element is dropped off and the new element is added to the
-	 * beginning.
+	 * full, the size of the list is increased by 50 percent of the original list
+	 * length and the new element is added to the list.
 	 * 
 	 * @param newElement This is the new element being added to the list
 	 */
 	public void add(int newElement) {
-		int[] tempArray = new int[10];
-		if (count == 10) {
-			list[9] = 0;
-			count--;
+		int[] tempArray = new int[list.length];
+		int size = (int) (list.length + (list.length * .5));
+		if (count == list.length) {
+			list = Arrays.copyOf(list, size);
+			tempArray = new int[list.length];
 		}
 		for (int index = 0; index < list.length - 1; index++) {
 			tempArray[index + 1] = list[index];
@@ -58,23 +62,31 @@ public class SimpleList {
 	/**
 	 * 
 	 * This method removes an element from the list. Once the element is removed,
-	 * the list is shifted down one to fill the empty space. 
+	 * the list is shifted down one to fill the empty space. If the list has more
+	 * than 25 percent empty places, the size is reduced.
 	 * 
 	 * @param element This is the element that is to be removed from the list.
 	 */
 	public void remove(int element) {
 		int target = 0;
 		for (int index = 0; index < list.length; index++) {
-			if (list[index] == element) {
+			if (element == list[index]) {
 				count--;
 				target = index;
 				for (int targetIndex = target; targetIndex < list.length; targetIndex++) {
-					if (targetIndex == 9) {
-						list[9] = 0;
+					if (targetIndex == list.length - 1) {
+						list[list.length - 1] = 0;
 					} else {
 						list[targetIndex] = list[targetIndex + 1];
 					}
 				}
+			}
+		}
+		int percent = (int) (list.length * .25);
+		int emptySpaces = list.length - count;
+		if (emptySpaces > percent) {
+			if(list.length - percent > 0) {
+				list = Arrays.copyOf(list, list.length - percent);
 			}
 		}
 	}
